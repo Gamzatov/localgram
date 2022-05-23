04af6fea008257a32b3c52276ef4f6631fcf1b8a
Gamzatov <102423270+Gamzatov@users.noreply.github.com>
2022-05-23 15:34:32 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,5 +5,5 @@ import thunk from "redux-thunk";
 export const store = createStore(rootReducer,
     compose(
         applyMiddleware(
-            thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            thunk)
     ));
